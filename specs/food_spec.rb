2946e7bee62e0645977b4e247eeee1c5f7357ba3
grahamsawers <grahamsawers@gmail.com>
2019-08-07 15:15:22 +0100

    Adds food class.

---
+++ specs/food_spec.rb
@@ -0,0 +1,26 @@
+require('minitest/autorun')
+require('minitest/rg')
+require('pry')
+
+require_relative('../food')
+
+
+class FoodTest < Minitest::Test
+
+    def setup()
+        @food = Food.new("Fish and Chips", 7.50, 6)
+    end
+
+    def test_food_name()
+        assert_equal("Fish and Chips", @food.name())
+    end
+
+    def test_food_price()
+        assert_equal(7.50, @food.price())
+    end
+
+    def test_food_rejuvenation_level()
+        assert_equal(6, @food.rejuvenation_level())
+    end
+
+end
